f74983619faf0143a63d4bebeb0fe4dd79f7fb3b
Marcelo <marcelo.cepeda17@gmail.com>
2024-06-20 10:38:39 -0300

    Adding .gitignore

--- MiApi/obj/Debug/net8.0/MiApi.AssemblyInfo.cs
+++ MiApi/obj/Debug/net8.0/MiApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8196dd9493afe1deb96eb6beba676c54b3f4379")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
